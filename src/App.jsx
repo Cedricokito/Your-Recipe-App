c75e76361ac5917ba4c0cbdcd98154edb2d7c1a3
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2024-04-29 04:56:29 +0500

    route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { RecipeListPage } from './pages/RecipeListPage';
-import "./index.css"
-import { Route, Routes } from 'react-router';
-import RecipePage from './pages/RecipePage';
+import React from "react";
+import { RecipeListPage } from "./pages/RecipeListPage";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import RecipePage from "./pages/RecipePage";
 export const App = () => {
-  return(
+  return (
     <>
-    <Routes>
-    <Route path='/' Component={RecipeListPage}/>
-      <Route path='/recipepage' Component={RecipePage}/>
-    </Routes>
-
+      <Routes>
+        <Route path='/' element={<RecipeListPage />} />
+        <Route path='/recipepage' element={<RecipePage />} />
+      </Routes>
     </>
   );
-
-
 };
